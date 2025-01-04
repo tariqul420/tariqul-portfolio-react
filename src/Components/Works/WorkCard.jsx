ce8169e2f28bc542b530f8c68ff729c8be1bff54
tariqul420 <tariqul.islam.programmer@gmail.com>
2025-01-04 15:57:22 +0600

    work section done

---
+++ src/Components/Works/WorkCard.jsx
@@ -0,0 +1,45 @@
+import PropTypes from "prop-types";
+import { FaEye, FaGithub, FaLink } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const WorkCard = ({ work }) => {
+    const { image, heading, paragraph, tools, live, github } = work || {};
+    console.log(work);
+
+    return (
+        <div className="flex flex-col border rounded-[16px] w-full dark:border-[#242222]">
+            <img src={image} className="max-h-[200px] min-h-[200px] w-full object-cover rounded-t-[16px]" />
+            <div className="flex flex-col p-[14px]">
+                <h1 className="font-medium text-[#172635] text-start text-splice dark:text-white-lite">{heading}</h1>
+                <p className="text-xs text-[#172635]/60 text-start mt-[12px] dark:text-white-deep">{paragraph}</p>
+                <div className="flex gap-[6px] mt-[8px]">
+                    {tools.map((tool, index) => (
+                        <span key={index} className="language">{tool?.name}</span>
+                    ))}
+                </div>
+                <div className="flex justify-between items-center mt-5 cursor-pointer dark:text-white-deep">
+                    <a href={live} target="_blank" className="flex items-center gap-[6px]">
+                        <FaLink />
+                        <span className="text-[12px] underline">Live Preview</span>
+                    </a>
+                    <div className="flex items-center gap-[10px]">
+                        <a href={github} target="_blank" className="flex items-center gap-[4px]">
+                            <FaGithub />
+                            <span className="text-[12px] underline">Github</span>
+                        </a>
+                        <Link className="flex items-center gap-[4px]">
+                            <FaEye />
+                            <span className="text-[12px] underline">Details</span>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+WorkCard.propTypes = {
+    work: PropTypes.object,
+}
+
+export default WorkCard;
--- src/Pages/Works/Works.jsx
+++ src/Pages/Works/Works.jsx
@@ -1,8 +1,34 @@
-const Works = () => {
-    return (
-        <div>
+import axios from "axios";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import { FaArrowRight } from "react-icons/fa";
+import WorkCard from "../../Components/Works/WorkCard";
 
-        </div>
+const Works = () => {
+    const [works, setWorks] = useState([]);
+
+    useEffect(() => {
+        try {
+            axios.get('/works.json')
+                .then(res => setWorks(res?.data))
+        } catch (error) {
+            toast.error(error.code);
+        }
+    }, []);
+
+    return (
+        <section className="flex flex-col w-11/12 mx-auto mb-28 mt-16 items-center justify-center">
+            <button className="px-4 py-2 bg-[#E2E8F0] rounded-lg mb-8 flex items-center gap-2 dark:bg-[#2D2D2D]">
+                <FaArrowRight /> Works
+            </button>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                {
+                    works.map((work, index) => <WorkCard key={index} work={work} />)
+                }
+            </div>
+
+        </section>
     );
 };
 
