07481efae3a9e6c772b732e8e325c3f41ed0e05c
tariqul420 <tariqul.islam.programmer@gmail.com>
2025-01-05 08:27:20 +0600

    contact me contact page responsive done

--- src/Pages/Contact/ContactMe.jsx
+++ src/Pages/Contact/ContactMe.jsx
@@ -12,39 +12,37 @@ const ContactMe = () => {
             </button>
 
             <div className="flex gap-12 flex-col lg:flex-row items-center justify-center">
-                <div className="bg-white p-10 rounded-xl dark:bg-[#181818] lg:min-h-[437px] w-11/12 mx-auto items-start flex flex-col justify-center">
-                    <ul className="space-y-3">
-                        <li className="flex items-center text-lg font-medium gap-2 cursor-pointer dark:text-white-deep">
+                <div className="bg-white p-6 sm:p-8 lg:p-10 rounded-xl dark:bg-[#181818] items-start flex flex-col justify-center sm:min-h-[350px] lg:min-h-[437px]">
+                    <ul className="space-y-2 sm:space-y-3">
+                        <li className="flex items-center text-sm sm:text-base lg:text-lg font-medium gap-2 cursor-pointer dark:text-white-deep">
                             <FaPhone />
-                            <span>+8801743892058</span>
+                            <span className="break-words">+8801743892058</span>
                         </li>
-                        <li className="flex items-center text-lg font-medium gap-2 cursor-pointer dark:text-white-deep">
+                        <li className="flex items-center text-sm sm:text-base lg:text-lg font-medium gap-2 cursor-pointer dark:text-white-deep">
                             <BiLogoGmail />
-                            <span>tariqul.developer@gmail.com</span>
+                            <span className="break-words">tariqul.developer@gmail.com</span>
                         </li>
-                        <li className="flex items-center text-lg font-medium gap-2 cursor-pointer dark:text-white-deep">
+                        <li className="flex items-center text-sm sm:text-base lg:text-lg font-medium gap-2 cursor-pointer dark:text-white-deep">
                             <FaLocationDot />
-                            <span>Pabna, Bangladesh</span>
+                            <span className="break-words">Pabna, Bangladesh</span>
                         </li>
                     </ul>
-                    <div>
-                        <div className="flex gap-3 items-center justify-center lg:justify-start mt-12">
-                            <a href="https://www.linkedin.com/in/tariqul420/" target="_blank">
-                                <FaLinkedin className="hero-icon lang" />
-                            </a>
-                            <a href="https://github.com/tariqul420" target="_blank">
-                                <FaGithub className="hero-icon lang" />
-                            </a>
-                            <a href="https://www.facebook.com/tariqul.islam.fb" target="_blank">
-                                <FaFacebook className="hero-icon lang" />
-                            </a>
-                            <a href="meilto:tariqul.developer@gmail.com" target="_blank">
-                                <IoMdMail className="hero-icon lang" />
-                            </a>
-                            <a href="meilto:tariqul.developer@gmail.com" target="_blank">
-                                <IoLogoWhatsapp className="hero-icon lang" />
-                            </a>
-                        </div>
+                    <div className="flex gap-2 sm:gap-3 items-center justify-center lg:justify-start mt-8 sm:mt-12">
+                        <a href="https://www.linkedin.com/in/tariqul420/" target="_blank">
+                            <FaLinkedin className="hero-icon lang" />
+                        </a>
+                        <a href="https://github.com/tariqul420" target="_blank">
+                            <FaGithub className="hero-icon lang" />
+                        </a>
+                        <a href="https://www.facebook.com/tariqul.islam.fb" target="_blank">
+                            <FaFacebook className="hero-icon lang" />
+                        </a>
+                        <a href="mailto:tariqul.developer@gmail.com" target="_blank">
+                            <IoMdMail className="hero-icon lang" />
+                        </a>
+                        <a href="https://wa.me/8801743892058" target="_blank">
+                            <IoLogoWhatsapp className="hero-icon lang" />
+                        </a>
                     </div>
                 </div>
 
